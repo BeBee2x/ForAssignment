c54ee915be1eace9a80abcf6d9b11b5b997f5d05
BeBee2x <118718690+BeBee2x@users.noreply.github.com>
2023-01-02 20:34:14 +0630

    Add files via upload

--- main.c
+++ main.c
@@ -1,75 +1,75 @@
 #include "stdio.h"
 #include "stdlib.h"
+#define maxstack 10
 
+struct stack{
+    int data[maxstack];
+    int top;
+};
 
-int mini(int first,int second);
-int jumpsearch(int array[],int size,int tofind);
+typedef struct stack st;
+
+void createEmptyStack(st* myStack){
+    myStack->top=-1;
+}
+
+int isStackisFull(st* myStack){
+    if (myStack->top==maxstack-1){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+int isStackisEmpty(st* mystack){
+    if (mystack->top==-1){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+int pushToStack(st* myStack,int data){
+    if (isStackisFull(myStack)){
+        printf("Stack is full\n");
+    }else{
+        myStack->top++;
+        myStack->data[myStack->top]=data;
+    }
+}
+
+int popfromstack(st* myStack){
+    if(isStackisEmpty(myStack)){
+        printf("Stack is empty\n");
+    }else{
+        printf("%d\n",myStack->data[myStack->top]);
+        myStack->top--;
+    }
+}
+void dataFromstack(st* myStack){
+    if (isStackisEmpty(myStack)){
+        printf("Stack is Empty\n");
+    }else{
+        printf("There is %d data in stack\n",(myStack->top)+1);
+        for (int i = myStack->top; i >-1 ; i--) {
+            printf("Data at %d stack is %d\n",i,myStack->data[i]);
+        }
+    }
+}
 
 int main(){
-    int key=1;
-    int array[]={1,3,4,5,7,8,10,14,15,18,31,43,54,55,60,79,88,89,90,93,94,95,100};
-    int size=sizeof(array)/sizeof(array[0]);
-    int tofind;
-    while(key==1){
-        printf("Which number do you want to find:");
-        scanf("%d",&tofind);
-        int index=jumpsearch(array,size,tofind);
-        if(index!=-1){
-            printf("We found %d at index %d\n",array[index],index);
-        }else{
-            printf("Not found %d\n",tofind);
-        }
-    }
-}
-
-int mini(int first,int second){
-    if(first<second){
-        return first;
-    }else{
-        return second;
-    }
-}
-
-int jumpsearch(int array[],int size,int tofind){
-    int previous=0;
-    int step=size/3;
-    if(array[size-1]<tofind){
-        return -1;
-    }
-    while(array[mini(step,size)-1]<tofind){
-        previous=step;
-        step+=size/3;
-        if(previous>=size){
-            return -1;
-        }
-    }
-    if(step<=size){
-        if(array[step-1]==tofind){
-            return step-1;
-        }
-    }
-    int step2=previous+(size/3/2);
-    if(step2>size){
-        for (int k = previous; k <size; ++k) {
-            if(array[k]==tofind){
-                return k;
-            }
-        }
-    }else if(array[step2]==tofind){
-        return step2;
-    }else if(array[step2]<tofind){
-        for (int i = step2; i <step ; ++i) {
-            if(array[i]==tofind){
-                return i;
-            }
-        }
-    }else if(array[step2]>tofind){
-        for (int j = previous; j <step2 ; ++j) {
-            if(array[j]==tofind){
-                return j;
-            }
-        }
-    }
-
-    return -1;
+    st* myStack= malloc(sizeof(st));
+    createEmptyStack(myStack);
+    pushToStack(myStack,10);
+    pushToStack(myStack,20);
+    pushToStack(myStack,30);
+    pushToStack(myStack,40);
+    pushToStack(myStack,50);
+    pushToStack(myStack,60);
+    pushToStack(myStack,70);
+    pushToStack(myStack,80);
+    pushToStack(myStack,90);
+    pushToStack(myStack,100);
+    dataFromstack(myStack);
+    return 0;
 }
